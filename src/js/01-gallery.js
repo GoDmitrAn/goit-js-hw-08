2e7a492fd6b49c13f732b7824e2757efbda8fab9
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-08-19 03:17:30 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.css'
+import '../css/common.css';
+import '../css/01-gallery.css'
+
+const mainGalleryTask2 = document.querySelector('.gallery');
+function createItemTemplateTask2({preview, original,description}) {
+    
+    const template = `<div class="gallery__item"><a class="gallery__link" href="${original}">
+                            <img class="gallery__image"
+                                src="${preview}" 
+                                alt="${description}" 
+                                
+                            />
+                            </a>
+                        </div>` ;
+    return template;
+}
+function renderGalleryItemsTask2() {
+    const fullTemplate = galleryItems.reduce((acc, item) => `${acc} ${createItemTemplateTask2(item)}`, '')
+    mainGalleryTask2.insertAdjacentHTML('beforeend', fullTemplate);
+}
+renderGalleryItemsTask2();
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt" , 
+    captionDelay: 250,
+});
+
+
 
 console.log(galleryItems);

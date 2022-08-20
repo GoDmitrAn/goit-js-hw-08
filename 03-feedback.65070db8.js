e46262e7d8dbefb149cc7245a5d5e27c71644078
GoDmitrAn <99049391+GoDmitrAn@users.noreply.github.com>
2022-08-20 23:42:17 +0000

    Deploying to gh-pages from @ GoDmitrAn/goit-js-hw-08@1f180d0b317aa8e08b7ee15cbfa9d1d46ad96f32 🚀

--- 03-feedback.65070db8.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.forms["feedback-form"];console.log(e.elements)}();
-//# sourceMappingURL=03-feedback.65070db8.js.map
--- 03-feedback.77bcf94e.js
+++
@@ -1,2 +0,0 @@
-const e=document.forms["feedback-form"];console.log(e.elements);
-//# sourceMappingURL=03-feedback.77bcf94e.js.map

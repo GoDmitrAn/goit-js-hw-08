7f68fe488837a299404cb9a111d64660a2e64d2a
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-08-20 17:48:08 +0300

    task2 complete
    add save time in local storage and throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+setPlayerTime = localStorage.getItem("videoplayer-current-time") === ''
+    ? ''
+    : localStorage.getItem("videoplayer-current-time");
+
+player.setCurrentTime(setPlayerTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+        default:
+            break;
+    }
+});
+    
+
+const onPlay =throttle(function(data) {
+    
+    localStorage.setItem("videoplayer-current-time",data.seconds)
+    console.log(data.seconds);
+},1000);
+player.on('timeupdate', onPlay);
 
